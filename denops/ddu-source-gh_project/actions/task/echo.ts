17ab64d7b67b0f3667babe9675b45141b7e96c2b
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-06 00:32:26 +0900

    update: migrate echo action from gh_project_task

---
+++ denops/ddu-source-gh_project/actions/task/echo.ts
@@ -0,0 +1,14 @@
+import {
+  ActionArguments,
+  ActionFlags,
+} from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
+import { KindParams as Params } from "../../type/common.ts";
+import { KindActionData as ActionData } from "../../type/task.ts";
+
+export function echo(args: ActionArguments<Params>): Promise<ActionFlags> {
+  for (const item of args.items) {
+    const action = item.action as ActionData;
+    console.log(`title: "${action.title}"`);
+  }
+  return Promise.resolve(ActionFlags.None);
+}
