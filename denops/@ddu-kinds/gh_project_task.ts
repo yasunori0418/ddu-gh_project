121013fef8391cfadc9706c0378b1a45c0f91e67
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-06 00:35:56 +0900

    update:

--- denops/@ddu-kinds/gh_project_task.ts
+++ denops/@ddu-kinds/gh_project_task.ts
@@ -1,102 +1,23 @@
 import {
-  ActionArguments,
-  ActionFlags,
+  // ActionArguments,
+  // ActionFlags,
+  Actions,
   BaseKind,
-  DduItem,
+  // DduItem,
   // PreviewContext,
   // Previewer,
 } from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
-import {
-  autocmd,
-  Denops,
-  fn,
-} from "https://deno.land/x/ddu_vim@v3.10.2/deps.ts";
-import { stringify as tomlStringify } from "https://deno.land/std@0.218.2/toml/mod.ts";
-import {
-  BufInfo,
-  KindActionData as ActionData,
-  TaskEdit,
-  TaskField,
-} from "../ddu-source-gh_project/type/task.ts";
+import { KindParams as Params } from "../ddu-source-gh_project/type/common.ts";
+import { edit } from "../ddu-source-gh_project/actions/task/edit.ts";
+import { echo } from "../ddu-source-gh_project/actions/task/echo.ts";
 
-type Params = Record<never, never>;
-
-type ActionFunction = (args: ActionArguments<Params>) => Promise<ActionFlags>;
-
-function defineAutocmd(
-  denops: Denops,
-  bufnr: number,
-  ctx: string,
-) {
-  autocmd.define(denops, "QuitPre", `<buffer=${bufnr}>`, ctx, {
-    once: true,
-  });
-}
-
-function createTomlData(action: ActionData): string[] {
-  const task: TaskEdit = {
-    projectId: action.projectId,
-    taskId: action.taskId,
-    title: action.title,
-    body: action.body.split(/\n/),
-    currentStatus: action.currentStatus,
-    taskFields: [],
-  };
-
-  for (const field of action.fields) {
-    const taskField: TaskField = {
-      id: field.id,
-      name: field.name,
-    };
-    if (!field.options) taskField.text = "";
-    if (field.options) {
-      taskField.options = [];
-      for (const option of field.options) {
-        let currentStatusFlag = undefined;
-        if (field.name === "Status" && option.name === action.currentStatus) {
-          currentStatusFlag = true;
-        }
-        taskField.options.push({
-          id: option.id,
-          name: option.name,
-          currentStatusFlag,
-        });
-      }
-    }
-    task.taskFields.push(taskField);
-  }
-
-  return tomlStringify(task).split(/\n/);
-}
+const actions: Actions<Params> = {
+  edit,
+  echo,
+};
 
 export class Kind extends BaseKind<Params> {
-  actions: Record<string, ActionFunction> = {
-    echo: (args: { items: DduItem[] }) => {
-      for (const item of args.items) {
-        const action = item.action as ActionData;
-        console.log(`title: "${action.title}"`);
-      }
-      return Promise.resolve(ActionFlags.None);
-    },
-    edit: async (args: { items: DduItem[]; denops: Denops }) => {
-      const denops = args.denops;
-      const action = args.items[0].action as ActionData;
-      const { bufnr, bufname } = await denops.call(
-        "ddu_source_gh_project#create_scratch_buffer",
-        action.taskId,
-      ) as BufInfo;
-      await fn.appendbufline(denops, bufname, 0, createTomlData(action));
-
-      defineAutocmd(denops, bufnr, `call ddu_source_gh_project#send(${bufnr})`);
-
-      denops.call(
-        "ddu_source_gh_project#open_buffer",
-        bufnr,
-        "horizontal",
-      ) as Promise<void>;
-      return Promise.resolve(ActionFlags.None);
-    },
-  };
+  override actions = actions;
 
   // override getPreviewer(args: {
   //   denops: Denops;
