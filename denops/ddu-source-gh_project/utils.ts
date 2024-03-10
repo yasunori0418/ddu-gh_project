b29897e35484b61d21862f0876ff7b313131ae81
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-10 20:55:50 +0900

    feat: Implement general-purpose command execution functions

--- denops/ddu-source-gh_project/utils.ts
+++ denops/ddu-source-gh_project/utils.ts
@@ -8,3 +8,25 @@ export type Overwrite<T, U extends { [Key in keyof T]?: unknown }> =
     keyof U
   >
   & U;
+
+/**
+ * Reference: https://github.com/kyoh86/denops-util/blob/622d3b7/command.ts#L36
+ */
+export function cmd(
+  command: string | URL,
+  options?: Omit<Deno.CommandOptions, "stdin" | "stderr" | "stdout">,
+) {
+  const { stdout } = new Deno.Command(command, {
+    ...options,
+    stdin: "null",
+    stderr: "null",
+    stdout: "piped",
+  }).spawn();
+
+  return {
+    pipeOut: stdout.pipeThrough(new TextDecoderStream()),
+    finalize: async () => {
+      await stdout.cancel();
+    },
+  };
+}
