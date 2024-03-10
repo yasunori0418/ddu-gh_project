251c98c8fa6b10c542ab056de6fc42f32a739abc
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-10 16:13:14 +0900

    fix: resolve type definition file path.

--- denops/ddu-source-gh_project/main.ts
+++ denops/ddu-source-gh_project/main.ts
@@ -1,7 +1,7 @@
 import { Denops } from "https://deno.land/x/denops_std@v6.3.0/mod.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.16.3/mod.ts";
 import { parse as tomlParse } from "https://deno.land/std@0.218.2/toml/mod.ts";
-import { TaskEdit } from "../@ddu-kinds/gh_project_task.ts";
+import { TaskEdit } from "./type/task.ts";
 
 export function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
