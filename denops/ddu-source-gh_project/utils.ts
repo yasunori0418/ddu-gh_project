1e9da86789e97e537e2921bc68d684fad6315989
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-10 20:55:01 +0900

    docs: japanese to english comment.

--- denops/ddu-source-gh_project/utils.ts
+++ denops/ddu-source-gh_project/utils.ts
@@ -1,5 +1,5 @@
 /**
- * 特定のプロパティを上書きする型関数
+ * Type functions that override certain properties
  * Reference: https://qiita.com/ibaragi/items/2a6412aeaca5703694b1
  */
 export type Overwrite<T, U extends { [Key in keyof T]?: unknown }> =
