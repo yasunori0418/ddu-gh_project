16e231eeff3b0bdab77e2733c0f1df92d0c31efe
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-14 20:26:46 +0900

    update:

--- denops/ddu-source-gh_project/deps.ts
+++ denops/ddu-source-gh_project/deps.ts
@@ -2,9 +2,18 @@ export {
   type ActionArguments,
   ActionFlags,
   BaseKind,
+  BaseSource,
+  type DduOptions,
+  type SourceOptions,
+  type Item,
   type DduItem,
   type PreviewContext,
   type Previewer,
   type Actions,
 } from "https://deno.land/x/ddu_vim@v3.10.3/types.ts";
+export {
+  type GatherArguments,
+} from "https://deno.land/x/ddu_vim@v3.10.3/base/source.ts";
 export { type Denops } from "https://deno.land/x/ddu_vim@v3.10.3/deps.ts";
+
+export { JSONLinesParseStream } from "https://deno.land/x/jsonlines@v1.2.2/mod.ts";
