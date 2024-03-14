dd48d2a41ea77b092b2d8ae70499e66abd2db934
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-14 20:32:48 +0900

    update:

--- denops/ddu-source-gh_project/deps.ts
+++ denops/ddu-source-gh_project/deps.ts
@@ -16,6 +16,8 @@ export {
 } from "https://deno.land/x/ddu_vim@v3.10.3/base/source.ts";
 export { type Denops, autocmd, fn } from "https://deno.land/x/ddu_vim@v3.10.3/deps.ts";
 
+export { ensure, is } from "https://deno.land/x/unknownutil@v3.17.0/mod.ts";
+
 export { JSONLinesParseStream } from "https://deno.land/x/jsonlines@v1.2.2/mod.ts";
 
-export { stringify as tomlStringify } from "https://deno.land/std@0.219.1/toml/mod.ts";
+export { stringify as tomlStringify,  parse as tomlParse } from "https://deno.land/std@0.219.1/toml/mod.ts";
