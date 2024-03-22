bc76a04aacac2e61b34255c2f228446b13c591ca
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-22 23:39:33 +0900

    update: open task list by default value

--- denops/ddu-gh_project/actions/project/open_task_list.ts
+++ denops/ddu-gh_project/actions/project/open_task_list.ts
@@ -1,25 +1,27 @@
-import { ActionArguments, ActionFlags } from "../../deps.ts";
+import { ActionArguments, ActionFlags, DduOptions } from "../../deps.ts";
 import { ActionData, KindParams as Params } from "../../type/project.ts";
 import { SourceParams as TaskSourceParams } from "../../type/task.ts";
 
 export async function openTaskList(
   args: ActionArguments<Params>,
 ): Promise<ActionFlags> {
-  const denops = args.denops;
   const action = args.items[0].action as ActionData;
-  const dduOptions = args.kindParams.dduOptions;
-  const ghProjectTaskSourceParams = dduOptions.sourceParams
-    .gh_project_task as TaskSourceParams;
+  // const dduOptions = args.kindParams.dduOptions;
+  // const ghProjectTaskSourceParams = dduOptions?.sourceParams
+  //   .gh_project_task as TaskSourceParams;
   const selectProjectSourceParams: TaskSourceParams = {
-    limit: ghProjectTaskSourceParams.limit ?? 1000,
+    limit: 1000,
     owner: action.ownerLogin,
     projectId: action.id,
     projectNumber: action.number,
   };
-  dduOptions.ui = "ff";
-  dduOptions.sources = ["gh_project_task"];
-  dduOptions.sourceParams.gh_project_task = selectProjectSourceParams;
-  denops.dispatch("ddu", "start", dduOptions);
+  const option: Partial<DduOptions> = {
+    ui: "ff",
+    sources: [
+      { name: "gh_project_task", params: selectProjectSourceParams },
+    ],
+  };
+  args.denops.dispatch("ddu", "start", option);
 
   return await Promise.resolve(ActionFlags.None);
 }
