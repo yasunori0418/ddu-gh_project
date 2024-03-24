f71e6c072a8af1980898b6588dfa85591ea7b6a5
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-24 23:55:34 +0900

    feat: getProjectItems

--- denops/@ddu-sources/gh_project.ts
+++ denops/@ddu-sources/gh_project.ts
@@ -1,12 +1,13 @@
 import {
   BaseSource,
+  Denops,
   GatherArguments,
   Item,
   JSONLinesParseStream,
 } from "../ddu-gh_project/deps.ts";
 import {
-  GHProject,
   ActionData,
+  GHProject,
   SourceParams as Params,
 } from "../ddu-gh_project/type/project.ts";
 import { getGHCmd } from "../ddu-gh_project/utils.ts";
@@ -52,6 +53,46 @@ function parseGHProjectItem(project: GHProject): Item<ActionData> {
   };
 }
 
+async function getProjectItems(
+  denops: Denops,
+  sourceParams: Params,
+): Promise<Item<ActionData>[]> {
+  const ghCmd = await getGHCmd(denops);
+  const { stdout } = new Deno.Command(ghCmd, {
+    args: [
+      "project",
+      "list",
+      "--owner",
+      sourceParams.owner,
+      "--limit",
+      sourceParams.limit.toString(),
+      "--format",
+      "json",
+    ],
+    stdin: "null",
+    stderr: "null",
+    stdout: "piped",
+  }).spawn();
+
+  const projectItems: Item<ActionData>[] = [];
+  await stdout
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new JSONLinesParseStream())
+    .pipeTo(
+      new WritableStream<{ projects: GHProject[] }>({
+        write(chunk: { projects: GHProject[] }) {
+          for (const project of chunk.projects) {
+            projectItems.push(parseGHProjectItem(project));
+          }
+        },
+      }),
+    ).finally(async () => {
+      await stdout.cancel();
+    });
+
+  return projectItems;
+}
+
 export class Source extends BaseSource<Params> {
   override kind = "gh_project";
 
@@ -60,37 +101,8 @@ export class Source extends BaseSource<Params> {
   ): ReadableStream<Item<ActionData>[]> {
     return new ReadableStream({
       async start(controller) {
-        const ghCmd = await getGHCmd(denops);
-        const { stdout } = new Deno.Command(ghCmd, {
-          args: [
-            "project",
-            "list",
-            "--owner",
-            sourceParams.owner,
-            "--limit",
-            sourceParams.limit.toString(),
-            "--format",
-            "json",
-          ],
-          stdin: "null",
-          stderr: "null",
-          stdout: "piped",
-        }).spawn();
-
-        await stdout
-          .pipeThrough(new TextDecoderStream())
-          .pipeThrough(new JSONLinesParseStream())
-          .pipeTo(
-            new WritableStream<{ projects: GHProject[] }>({
-              write(chunk: { projects: GHProject[] }) {
-                controller.enqueue(
-                  chunk.projects.map((project) => parseGHProjectItem(project)),
-                );
-              },
-            }),
-          ).finally(async () => {
-            await stdout.cancel();
-          });
+        const projectItems = await getProjectItems(denops, sourceParams);
+        controller.enqueue(projectItems);
         controller.close();
       },
     });
