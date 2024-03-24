c3c218e174b674d4c0282cce7d650fd4bde9305a
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-03-24 22:34:53 +0900

    delete: unused type function `Overwrite`.

--- denops/ddu-gh_project/utils.ts
+++ denops/ddu-gh_project/utils.ts
@@ -2,17 +2,6 @@ import { autocmd, Denops, fn, vars } from "./deps.ts";
 import { BufInfo } from "./type/task.ts";
 
 /**
- * Type functions that override certain properties
- * Reference: https://qiita.com/ibaragi/items/2a6412aeaca5703694b1
- */
-export type Overwrite<T, U extends { [Key in keyof T]?: unknown }> =
-  & Omit<
-    T,
-    keyof U
-  >
-  & U;
-
-/**
  * Reference: https://github.com/kyoh86/denops-util/blob/622d3b7/command.ts#L36
  */
 export function cmd(
